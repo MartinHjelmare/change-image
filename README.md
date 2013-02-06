9e3c14c4f3dd162a2853c2bcebe9737d5a89e3bb
MartinHjelmare <marhje52@kth.se>
2013-02-06 16:38:07 +0100

    Uploading script and sample images

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ change-image
 ============
 
 bash script to change meta data in an ome-tif image
+
+Requires:
+
+libtiff-tools
+imagemagick
+
+Install prerequisites on Ubuntu:
+
+sudo apt-get install libtiff-tools imagemagick
---
+++ change_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This script checks the meta data inside a tif image and does some manipulation, then saves as a new tif. More specifically it changes the xy position coordinates of the image.
+#Not done yet, need to incoorporate the coordinates by selecting well, through IF-statement. And rather use XSLT instead of sed.
+if [ $# -eq 0 ];
+then
+echo "Syntax: $(basename $0) image x-coordinate y-coordinate"
+exit 1
+fi
+image=$1
+xCoord=$2
+yCoord=$3
+tiffinfo $image | cat >/home/martin/Skrivbord/chcoord/before.txt
+tiffinfo $image | \
+sed -e 's/  ImageDescription: //' -e '1,/  Planar Configuration:/d' -e "s/PositionX=\".*\" PositionY=\".*\" /PositionX=\"$xCoord\" PositionY=\"$yCoord\" /" | \
+cat >/home/martin/Skrivbord/chcoord/after.txt
+convert -depth 16 -comment "@/home/martin/Skrivbord/chcoord/after.txt" $image $image".new"
