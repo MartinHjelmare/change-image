7cb575c4c60630057d0337e7f0bce664e9dc7cde
MartinHjelmare <marhje52@kth.se>
2013-02-07 20:19:33 +0100

    Changed to using Bioformats tools

--- change_image.sh
+++ change_image.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#This script checks the meta data inside a tif image and does some manipulation, then saves as a new tif. More specifically it changes the xy position coordinates of the image using arguments.
+#This script checks the meta data inside a tif image and does some manipulation, then injects the meta data inside an existing ome.tif image. More specifically it changes the xy position coordinates of the image using arguments.
 #Not done yet, need to incoorporate the coordinates by selecting well, through IF-statement. And rather use XSLT instead of sed.
 if [ $# -eq 0 ];
 then
 echo "Syntax: $(basename $0) image x-coordinate y-coordinate"
 exit 1
 fi
-source settings.cfg
+. "/home/martin/change-image/settings.cfg"
 image=$1
 xCoord=$2
 yCoord=$3
-tiffinfo $image | cat > $BEFORE
-tiffinfo $image | \
-sed -e 's/  ImageDescription: //' -e '1,/  Planar Configuration:/d' -e "s/PositionX=\".*\" PositionY=\".*\" /PositionX=\"$xCoord\" PositionY=\"$yCoord\" /" | \
+sh /home/martin/Skrivbord/bftools/tiffcomment $image | cat > $BEFORE
+sh /home/martin/Skrivbord/bftools/tiffcomment $image | \
+sed -e "s/PositionX=\".*\" PositionY=\".*\" /PositionX=\"$xCoord\" PositionY=\"$yCoord\" /" | \
 cat > $AFTER
-convert -depth 16 -comment "@$AFTER" $image $image".new"
+sh /home/martin/Skrivbord/bftools/tiffcomment -set "$AFTER" $image
