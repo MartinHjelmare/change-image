b6044cf1dbaab374d766f5b0adce0ba9f7d36bd5
MartinHjelmare <marhje52@kth.se>
2013-02-07 21:48:31 +0100

    Updated comment

--- change_image.sh
+++ change_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Change the path to "settings.cfg" to the correct one before running script!
+. "/home/martin/change-image/settings.cfg"
 #This script checks the meta data inside a tif image and does some manipulation, then injects the meta data inside an existing ome.tif image. More specifically it changes the xy position coordinates of the image using arguments.
 #Not done yet, need to incoorporate the coordinates by selecting well, through IF-statement. And rather use XSLT instead of sed.
 if [ $# -eq 0 ];
@@ -6,7 +8,6 @@ then
 echo "Syntax: $(basename $0) image x-coordinate y-coordinate"
 exit 1
 fi
-. "/home/martin/change-image/settings.cfg"
 image=$1
 xCoord=$2
 yCoord=$3
